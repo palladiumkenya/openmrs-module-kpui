52a3205acf51c9423012d617dcf4239de17cb38f
Rowan Seymour <rowanseymour@gmail.com>
2013-11-08 14:16:41 +0200

    Added ui.navigate js method to simplify 'location.href=??' code

--- omod/src/main/webapp/resources/scripts/ui.js
+++ omod/src/main/webapp/resources/scripts/ui.js
@@ -48,16 +48,23 @@ var ui = (function(jq) {
 				}
 			});
 		},
-		
+
+		/**
+		 * Reloads the current page
+		 */
 		reloadPage: function() {
-			location.href = location.href;
+			location.reload();
 		},
-		
-		escapeHtmlAttribute: function(string) {
-			// TODO actually implement this
-			string = string.replace("'", "\'");
-			string = string.replace('"', '\\"');
-			return string;
+
+		/**
+		 * Navigates to a new page
+		 * @param providerName the page provider or complete URL if no other args are provided
+		 * @param pageName the page name
+		 * @param options the page options
+		 */
+		navigate: function(providerName, pageName, options) {
+			// If a single argument, interpret as a complete URL
+			location.href = (arguments.length == 1) ? providerName : this.pageLink(providerName, pageName, options);
 		},
 		
 		confirmBeforeNavigating: function(formSelector) {
@@ -99,6 +106,13 @@ var ui = (function(jq) {
 		
 		enableConfirmBeforeNavigating: function() {
 			confirmBeforeNavigationSetup.enabled = true;
+		},
+
+		escapeHtmlAttribute: function(string) {
+			// TODO actually implement this
+			string = string.replace("'", "\'");
+			string = string.replace('"', '\\"');
+			return string;
 		},
 
 		notifySuccess: function(html) {

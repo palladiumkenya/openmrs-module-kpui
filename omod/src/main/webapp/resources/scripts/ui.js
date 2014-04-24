d34977a99ce89b2891cb4260553c8114341418f7
Rowan Seymour <rowanseymour@gmail.com>
2014-04-24 14:10:42 +0300

    Updated ui.js to use same namespacing syntax as kenyaui.js etc

--- omod/src/main/webapp/resources/scripts/ui.js
+++ omod/src/main/webapp/resources/scripts/ui.js
@@ -3,9 +3,7 @@
  *
  * This class mirrors the functionality of UiUtils.java and adds a few extras such as reloadPage() and navigate()
  */
-var ui = (function(jQuery) {
-
-	var _public = {};
+(function(ui, $) {
 
 	/**
 	 * Helper method to convert a list of parameters to a query string
@@ -37,7 +35,7 @@ var ui = (function(jQuery) {
 	 * @param params the query string parameters
 	 * @returns {string}
 	 */
-	_public.pageLink = function(providerName, pageName, params) {
+	ui.pageLink = function(providerName, pageName, params) {
 		var ret = '/' + OPENMRS_CONTEXT_PATH + '/' + providerName + '/' + pageName + '.page';
 		return ret + toQueryString(params);
 	};
@@ -48,14 +46,14 @@ var ui = (function(jQuery) {
 	 * @param resourcePath the resource path
 	 * @returns {string} the link
 	 */
-	_public.resourceLink = function(providerName, resourcePath) {
+	ui.resourceLink = function(providerName, resourcePath) {
 		if (providerName == null) {
 			providerName = '*';
 		}
 		return '/' + OPENMRS_CONTEXT_PATH + '/ms/uiframework/resource/' + providerName + '/' + resourcePath;
 	};
 
-	_public.fragmentActionLink = function(providerName, fragmentName, actionName, params) {
+	ui.fragmentActionLink = function(providerName, fragmentName, actionName, params) {
 		var ret = '/' + OPENMRS_CONTEXT_PATH + '/' + providerName + '/' + fragmentName + '/' + actionName + '.action';
 		if (params) {
 			ret += toQueryString(params);
@@ -63,10 +61,10 @@ var ui = (function(jQuery) {
 		return ret;
 	};
 
-	_public.getFragmentActionAsJson = function(providerName, fragmentName, actionName, params, callback) {
+	ui.getFragmentActionAsJson = function(providerName, fragmentName, actionName, params, callback) {
 		var url = this.fragmentActionLink(providerName, fragmentName, actionName, null);
 
-		jq.getJSON(url, params, function(result) {
+		$.getJSON(url, params, function(result) {
 			if (callback) {
 				callback(result);
 			}
@@ -76,7 +74,7 @@ var ui = (function(jQuery) {
 	/**
 	 * Reloads the current page
 	 */
-	_public.reloadPage = function() {
+	ui.reloadPage = function() {
 		location.reload();
 	};
 
@@ -86,24 +84,24 @@ var ui = (function(jQuery) {
 	 * @param pageName the page name
 	 * @param options the page options
 	 */
-	_public.navigate = function(providerName, pageName, options) {
+	ui.navigate = function(providerName, pageName, options) {
 		// If a single argument, interpret as a complete URL
 		location.href = (arguments.length == 1) ? providerName : this.pageLink(providerName, pageName, options);
 	};
 
-	_public.escapeHtmlAttribute = function(string) {
+	ui.escapeHtmlAttribute = function(string) {
 		// TODO actually implement this
 		string = string.replace("'", "\'");
 		string = string.replace('"', '\\"');
 		return string;
 	};
 
-	_public.confirmBeforeNavigating = function(formSelector) {
+	ui.confirmBeforeNavigating = function(formSelector) {
 		if (!confirmBeforeNavigationSetup.configured) {
 			window.onbeforeunload = function() {
 				if (confirmBeforeNavigationSetup.enabled) {
-					var blockers = jq('.confirm-before-navigating').filter(function() {
-						return (jq(this).data('confirmBeforeNavigating') === 'dirty');
+					var blockers = $('.confirm-before-navigating').filter(function() {
+						return ($(this).data('confirmBeforeNavigating') === 'dirty');
 					}).length > 0;
 
 					if (blockers) {
@@ -115,30 +113,28 @@ var ui = (function(jQuery) {
 			confirmBeforeNavigationSetup.enabled = true;
 		}
 
-		var form = jq(formSelector);
+		var form = $(formSelector);
 
 		form.addClass('confirm-before-navigating');
 		form.data('confirmBeforeNavigating', 'clean');
 		form.find(':input').on('change.confirm-before-navigating', function() {
-			jq(this).parents('.confirm-before-navigating').data('confirmBeforeNavigating', 'dirty');
+			$(this).parents('.confirm-before-navigating').data('confirmBeforeNavigating', 'dirty');
 		});
 	};
 
-	_public.cancelConfirmBeforeNavigating = function(formSelector) {
-		var form = jq(formSelector);
+	ui.cancelConfirmBeforeNavigating = function(formSelector) {
+		var form = $(formSelector);
 		form.find(':input').off('change.confirm-before-navigating');
 		form.data('confirmBeforeNavigating', null);
 		form.removeClass('confirm-before-navigating');
 	};
 
-	_public.disableConfirmBeforeNavigating = function() {
+	ui.disableConfirmBeforeNavigating = function() {
 		confirmBeforeNavigationSetup.enabled = false;
 	};
 
-	_public.enableConfirmBeforeNavigating = function() {
+	ui.enableConfirmBeforeNavigating = function() {
 		confirmBeforeNavigationSetup.enabled = true;
 	};
 
-	return _public;
-
-})(jQuery);
+}( window.ui = window.ui || {}, jQuery ));
